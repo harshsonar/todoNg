cf97632088c53651857f45c32a0b02cc829ca4fc
Harsh <harshsonar40@gmail.com>
2023-04-10 13:24:06 +0530

    first commit

---
+++ src/app/MyComponents/about/about.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent {
+
+  year: string = ' third year';
+  pNumber: string = '+91 7620435817';
+  email: string = 'harshsonar40@gmail.com';
+}
---
+++ src/app/MyComponents/add-todo/add-todo.component.ts
@@ -0,0 +1,26 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Todo } from '../../Todo'
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrls: ['./add-todo.component.css']
+})
+export class AddTodoComponent {
+
+  @Output() todoAdd :EventEmitter<Todo> = new EventEmitter();
+
+  title: string;
+  desc: string;
+
+  onSubmit() {
+    const todo = {
+      sno:8,
+      title: this.title,
+      desc: this.desc,
+      active: true
+    }
+
+    this.todoAdd.emit(todo);
+  }
+}
